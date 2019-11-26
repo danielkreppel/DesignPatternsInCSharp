9d3b6a8cb0fef8ab97a114045d0ef61b226bc5ac
Daniel Kreppel <Daniel.Kreppel@f1solutions.com.au>
2019-11-27 10:48:49 +1100

    Including the application with initial examples for Singleton and Factory design patterns

---
+++ DesignPatternsInCSharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using Factory;
+using Factory.Abstract;
+using System;
+
+namespace DesignPatternsInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestFactoryPattern();
+        }
+
+        static void TestFactoryPattern()
+        {
+            Console.WriteLine("TESTING THE FACTORY PATTERN: ");
+            var middlewareFactory = new MiddlewareFactory();
+            IMiddleware middleware = middlewareFactory.GetMiddleware(1);
+            middleware.DoAction(); 
+
+            middleware = middlewareFactory.GetMiddleware(2);
+            middleware.DoAction(); 
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+This is an application with examples of design patterns in C#
+
+Design Patterns:
+
+- Singleton
+- Factory
