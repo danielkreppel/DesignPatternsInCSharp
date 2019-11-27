58d394b6454b204fad120bd66c16e741f2652ad1
Daniel Kreppel <Daniel.Kreppel@f1solutions.com.au>
2019-11-27 11:45:42 +1100

    Inclusing an example of Memento Design Pattern

--- DesignPatternsInCSharp/Program.cs
+++ DesignPatternsInCSharp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Factory;
 using Factory.Abstract;
+using Memento;
 using System;
 
 namespace DesignPatternsInCSharp
@@ -9,19 +10,51 @@ namespace DesignPatternsInCSharp
         static void Main(string[] args)
         {
             TestFactoryPattern();
+
+            TestMementoPattern();
         }
 
         static void TestFactoryPattern()
         {
-            Console.WriteLine("TESTING THE FACTORY PATTERN: ");
+            Console.WriteLine("TESTING THE FACTORY DESIGN PATTERN: ");
+
             var middlewareFactory = new MiddlewareFactory();
+
             IMiddleware middleware = middlewareFactory.GetMiddleware(1);
+
             middleware.DoAction(); 
 
             middleware = middlewareFactory.GetMiddleware(2);
+
             middleware.DoAction(); 
 
             Console.ReadKey();
         }
+
+        static void TestMementoPattern()
+        {
+            Console.WriteLine("TESTING THE MEMENTO DESIGN PATTERN: ");
+
+            Originator<StateObject> current = new Originator<StateObject>();
+            current.SetState(new StateObject { Id = 1, Name = "Object 1" });
+            CareTaker<StateObject>.SaveState(current);
+            current.ShowState();
+
+            current.SetState(new StateObject { Id = 2, Name = "Object 2" });
+            CareTaker<StateObject>.SaveState(current);
+            current.ShowState();
+
+            current.SetState(new StateObject { Id = 3, Name = "Object 3" });
+            CareTaker<StateObject>.SaveState(current);
+            current.ShowState();
+
+            Console.WriteLine("Restoring the first state");
+
+            CareTaker<StateObject>.RestoreState(current, 0);
+            current.ShowState();
+
+            Console.ReadKey();
+        }
+
     }
 }
