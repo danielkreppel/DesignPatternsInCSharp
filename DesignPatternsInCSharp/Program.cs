55f588a261f076844f87fd94107aff1ef05149e3
Daniel Kreppel <Daniel.Kreppel@f1solutions.com.au>
2019-11-28 12:04:46 +1100

    Builder Design Pattern added

--- DesignPatternsInCSharp/Program.cs
+++ DesignPatternsInCSharp/Program.cs
@@ -1,4 +1,6 @@
-﻿using Factory;
+﻿using Builder.Abstract;
+using Builder.Concrete;
+using Factory;
 using Factory.Abstract;
 using Memento;
 using System;
@@ -9,9 +11,36 @@ namespace DesignPatternsInCSharp
     {
         static void Main(string[] args)
         {
-            TestFactoryPattern();
+            Console.WriteLine("Choose a Design Pattern to test:");
+            Console.WriteLine("1 - Factory");
+            Console.WriteLine("2 - Memento");
+            Console.WriteLine("3 - Builder");
 
-            TestMementoPattern();
+            Console.WriteLine("4 - EXIT!");
+            string option = Console.ReadLine(); 
+            while (!int.TryParse(option, out _) || int.Parse(option) < 1 || int.Parse(option) > 4)
+            {
+                Console.WriteLine("Invalid option.");
+
+                option = Console.ReadLine();
+            }
+
+
+            switch (int.Parse(option))
+            {
+                case 1: 
+                    TestFactoryPattern();
+                    break;
+                case 2:
+                    TestMementoPattern();
+                    break;
+                case 3:
+                    TestBuilderPattern();
+                    break;
+                default:
+                    return;
+            }
+            
         }
 
         static void TestFactoryPattern()
@@ -22,11 +51,11 @@ namespace DesignPatternsInCSharp
 
             IMiddleware middleware = middlewareFactory.GetMiddleware(1);
 
-            middleware.DoAction(); 
+            middleware.DoAction();
 
             middleware = middlewareFactory.GetMiddleware(2);
 
-            middleware.DoAction(); 
+            middleware.DoAction();
 
             Console.ReadKey();
         }
@@ -52,6 +81,24 @@ namespace DesignPatternsInCSharp
 
             CareTaker<StateObject>.RestoreState(current, 0);
             current.ShowState();
+
+            Console.ReadKey();
+        }
+
+        static void TestBuilderPattern()
+        {
+            Console.WriteLine("TESTING THE BUILDER DESIGN PATTERN: ");
+
+            Director director = new Director();
+
+            IBuilder b1 = new ConcreteBuilder1();
+            IBuilder b2 = new ConcreteBuilder2();
+
+            director.Construct(b1);
+            Console.WriteLine(b1.GetProduct());
+
+            director.Construct(b2);
+            Console.WriteLine(b2.GetProduct());
 
             Console.ReadKey();
         }
